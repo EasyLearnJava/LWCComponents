5737c7cdee9687c4787192753a68bf204a1000b8
Easy Learn Java <easylearnjava4u@gmail.com>
2021-07-22 16:50:13 +0000

    Initial Commit

--- src/app.js
+++ src/app.js
@@ -2,4 +2,9 @@ import { LightningElement } from "lwc";
 
 export default class App extends LightningElement {
   randomValue = "Lightning Web Components!";
+
+  handleUpdate(event){
+    this.randomValue = event.target.value;
+    console.log('Value entered : ' + this.randomValue);
+  }
 }
