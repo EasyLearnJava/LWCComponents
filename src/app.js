b1c165d7a9a3abc7400807ae8b807a1d097f1802
Easy Learn Java <easylearnjava4u@gmail.com>
2021-07-22 21:50:17 +0000

    Fix the bug, data not displaying

--- src/app.js
+++ src/app.js
@@ -1,5 +1,52 @@
-import { LightningElement } from "lwc";
+import { LightningElement, track } from "lwc";
 
 export default class App extends LightningElement {
-  title = "Welcome to Lightning Web Components!";
+  title = "You Need ToDo";
+
+   @track todo; 
+    @track todoArray = [];  
+
+    handleClick(){
+        const todoObj = {
+            aa: null,
+            bb: 'raghu'
+        }
+        todoObj.aa = Math.round(Math.random() * 100)
+        todoObj.bb = this.todo;
+
+        this.todoArray.unshift(todoObj);
+        //this.todoArray.forEach(element => console.log(element));
+        this.todoArray.forEach((element, index, array) => {
+    console.log(element.aa); // 100, 200, 300
+    console.log(index); // 0, 1, 2
+    console.log(array); // same myArray object 3 times
+});
+    }
+
+    handleChange(event){
+        this.todo = event.target.value;
+    }
+
+    areDetailsVisible = false;
+    handleChange(event) {
+        this.areDetailsVisible = event.target.checked;
+    }
+    ceoList = [
+          {
+            Id: 1,
+            Name: 'Marc Benioff',
+            Company: 'Salesforce.com',
+        },
+        {
+            Id: 2,
+            Name: 'Sundar Pichai',
+            Company: 'Google',
+        },
+      
+        {
+            Id: 3,
+            Name: 'Elon Musk',
+            Company: 'Telsa',
+        },
+    ];
 }
