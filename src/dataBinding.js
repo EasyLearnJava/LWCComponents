d3ee34885f64081ced7f18cfd7b97ff0561e2930
Easy Learn Java <easylearnjava4u@gmail.com>
2021-07-22 17:51:40 +0000

    Fixed JS error : Method name mismatch

--- src/dataBinding.js
+++ src/dataBinding.js
@@ -3,7 +3,7 @@ import { LightningElement } from "lwc";
 export default class DataBinding extends LightningElement {
   randomValue = "Lightning Web Components!";
 
-  handleUpdate(event){
+  handleChange(event){
     this.randomValue = event.target.value;
     console.log('Value entered : ' + this.randomValue);
   }
